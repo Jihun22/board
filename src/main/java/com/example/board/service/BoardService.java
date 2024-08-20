4c0296b87a0db26d129b0f19dcc3747740249794
YANGJIHUN <ik5334@naver.com>
2024-08-20 20:05:13 +0900

    삭제 구현

--- src/main/java/com/example/board/service/BoardService.java
+++ src/main/java/com/example/board/service/BoardService.java
@@ -59,5 +59,9 @@ public class BoardService {
         boardRepository.save(boardEntity);
         return  findById(boardDTO.getId());
     }
+
+    public void delete(Long id) {
+        boardRepository.deleteById(id);
+    }
 }
 
