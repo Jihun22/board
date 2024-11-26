67beaac2674273559fda544a987f8c468d4bf821
YANGJIHUN <ik5334@naver.com>
2024-11-26 15:10:39 +0900

    ajax 댓글 세팅

---
+++ src/main/java/com/example/board/dto/CommentDTO.java
@@ -0,0 +1,21 @@
+package com.example.board.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+public class CommentDTO {
+
+    private Long id;
+    private  String commentWriter;
+    private  String commentContent;
+    private  Long boardId;
+    private LocalDateTime commentCreatedTime;
+
+
+}
